d422201711fab4018f5ea3ae097483aee0f9b5f2
Viridesc3nt <97551484+Viridesc3nt@users.noreply.github.com>
2022-01-11 14:02:07 -0500

    Create DirtBlastListener.java

---
+++ DirtBlastListener.java
@@ -0,0 +1,28 @@
+package org.justinjaques.earthabilites;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.ability.CoreAbility;
+
+public class DirtBlastListener implements Listener {
+	
+	@EventHandler
+	public void onClick(PlayerInteractEvent event) {
+		if(event.getAction() != Action.LEFT_CLICK_AIR || event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+		
+		
+		Player player = event.getPlayer();
+		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		if(bPlayer.canBend(CoreAbility.getAbility(DirtBlast.class))) {
+			new DirtBlast(player);
+		}
+		
+		
+		
+	}
+}
