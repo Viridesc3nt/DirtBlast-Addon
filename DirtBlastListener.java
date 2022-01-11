93ff29d6882cdc724b076c9541bee7fa7908fbd7
Viridesc3nt <97551484+Viridesc3nt@users.noreply.github.com>
2022-01-11 17:32:14 -0500

    Update DirtBlastListener.java

--- DirtBlastListener.java
+++ DirtBlastListener.java
@@ -1,5 +1,6 @@
-package org.justinjaques.earthabilites;
+package me.justinjaques.dirtblast;
 
+import com.projectkorra.projectkorra.ProjectKorra;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,19 +11,19 @@ import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.ability.CoreAbility;
 
 public class DirtBlastListener implements Listener {
-	
-	@EventHandler
-	public void onClick(PlayerInteractEvent event) {
-		if(event.getAction() != Action.LEFT_CLICK_AIR || event.getAction() != Action.LEFT_CLICK_BLOCK) return;
-		
-		
-		Player player = event.getPlayer();
-		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
-		if(bPlayer.canBend(CoreAbility.getAbility(DirtBlast.class))) {
-			new DirtBlast(player);
-		}
-		
-		
-		
-	}
-}
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event) {
+        if(event.getAction() != Action.LEFT_CLICK_AIR || event.getAction() != Action.LEFT_CLICK_BLOCK){
+            Player player = event.getPlayer();
+            BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+
+           // if(bPlayer.canBend(CoreAbility.getAbility(DirtBlast.class))) {
+            new DirtBlast(player);
+            ProjectKorra.log.info("Dirtblast Fired!");
+            //}
+
+        } else {
+            return;
+
+        }
